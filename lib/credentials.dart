90b7733287786e5a0dca50733f7ff0f0e880b461
Am4d3us <59396573+Am4d3us@users.noreply.github.com>
2024-05-13 16:28:49 -0700

    added client secret

--- lib/credentials.dart
+++ lib/credentials.dart
@@ -1,3 +1,4 @@
 const String spotifyClientId = '0ac3d083c17746839cdfc568471786f0';
 const String spotifyClientSecret = 'd89a8f8ae9e74d4a97d5c894f47d6e67';
 const String spotifyRedirectUri = 'playlistpursuit:/';
+const String spotifyClientSecret = "d444b4f9d87a4961888a85a4a60d9722";
